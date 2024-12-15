3e86b24d437b94cda2a6d6ac60ed3f2128dc2e8b
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-12-15 01:32:26 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'html/about.html'), 
+        tours: resolve(__dirname, 'html/tour.html'), 
+        merch: resolve(__dirname, 'html/merch.html'), 
+        music: resolve(__dirname, 'html/music.html'), 
+      },
+    },
+  },
+})
