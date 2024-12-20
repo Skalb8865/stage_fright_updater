6654e34c920eb0fe61ade23db76cb3eeb47f188d
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-12-20 14:10:19 -0500

    .

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/general-sans.css'
 import '../styles/sentient.css'
+import '../styles/modal.css'
 
 import slider from './tour.js'
 slider();
