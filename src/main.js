a7214b545a381663034c41980c17054f33d54841
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-12-22 19:34:29 -0500

    made it so that the contact form actually works

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '../styles/style.css'
 import '../styles/general-sans.css'
 import '../styles/sentient.css'
 import '../styles/modal.css'
+import '../styles/contact.css'
 
 import slider from './tour.js'
 slider();
