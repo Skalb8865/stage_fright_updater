add88083f6a8714e2ecd2f01c02167f6cfbcb1b4
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-12-24 22:48:33 -0500

    changed contact page made it closer to finised

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/general-sans.css'
 import '../styles/sentient.css'
-import '../styles/modal.css'
-import '../styles/contact.css'
+// import '../styles/modal.css'
+// import '../styles/contact.css'
 
 import slider from './tour.js'
 slider();
