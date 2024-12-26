771066d272eade201714a6a6021f151f8017700b
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-12-25 20:02:04 -0500

    added swiper

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/general-sans.css'
 import '../styles/sentient.css'
 // import '../styles/modal.css'
 // import '../styles/contact.css'
+import '../styles/swiper.css'
 
 import slider from './tour.js'
 slider();
