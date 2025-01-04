f33155604fe03b65f7d97c5fc0626c596d0e9351
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-01-04 00:31:40 -0500

    .

--- src/swiper.js
+++ src/swiper.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
-    spaceBetween: 20,
+    spaceBetween: 10,
     loop: false,
     speed: 700,
     autoplay: {
@@ -18,15 +18,42 @@ const swiper = new Swiper('.swiper', {
     },
 
     breakpoints: {
+        // 877: {
+        //     slidesPerView: 2,
+        // },
+        // 1307: {
+        //     slidesPerView: 3,
+        // },
+        // 1737: {
+        //     slidesPerView: 4,
+        // },
+
         877: {
             slidesPerView: 2,
+            spaceBetween: 20,
         },
         1307: {
             slidesPerView: 3,
+            spaceBetween: 25,
         },
         1737: {
             slidesPerView: 4,
+            spaceBetween: 35,
         },
+        
+
+        // 640: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 20,
+        //   },
+        //   768: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 40,
+        //   },
+        //   1024: {
+        //     slidesPerView: 5,
+        //     spaceBetween: 50,
+        //   },
     },
 })
 
