6d0f9e6cafbd88fdfc86f4a3835fecfe028b5105
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-01-04 08:24:06 -0500

    .

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/fonts.css'
-// import '../styles/modal.css'
-// import '../styles/contact.css'
-// import '../styles/swiper.css'
 
-// import tourModal from './tour.js'
-// tourModal();
-// import mobileNav from './mobile-nav.js'
-// mobileNav();
+
 import swiper from './swiper.js'
 
 swiper();
