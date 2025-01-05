a5f43b1c7e80247dc7cac7f0765c664929798e0c
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-01-05 02:42:39 -0500

    .

--- src/mobileNav.js
+++ src/mobileNav.js
@@ -1,32 +1,40 @@
-const openButton = document.getElementById('open-sidebar-button')
-const navbar = document.getElementById('navbar')
+const openSidebarButton = document.getElementById('open-sidebar-button');
+const closeSidebarButton = document.getElementById('close-sidebar-button');
+const overlayClose = document.getElementById('overlay');
+const navbar = document.getElementById('navbar');
 
 const media = window.matchMedia("(width < 700px)")
 
 media.addEventListener('change', (e) => updateNavbar(e))
 
-function updateNavbar(e) {
+function updateNavbar(e){
   const isMobile = e.matches
   console.log(isMobile)
-  if (isMobile) {
+  if(isMobile){
     navbar.setAttribute('inert', '')
   }
-  else {
+  else{
     // desktop device
     navbar.removeAttribute('inert')
   }
 }
 
-function openSidebar() {
-  navbar.classList.add('show')
-  openButton.setAttribute('aria-expanded', 'true')
-  navbar.removeAttribute('inert')
-}
+openSidebarButton.addEventListener('click', function() {
+    navbar.classList.add('show');
+    openSidebarButton.setAttribute('aria-expanded', 'true')
+    navbar.removeAttribute('inert')
+});
 
-function closeSidebar() {
+closeSidebarButton.addEventListener('click', function() {
   navbar.classList.remove('show')
-  openButton.setAttribute('aria-expanded', 'false')
+  closeSidebarButton.setAttribute('aria-expanded', 'false')
   navbar.setAttribute('inert', '')
-}
+});
+
+overlayClose.addEventListener('click', function() {
+  navbar.classList.remove('show')
+  overlayClose.setAttribute('aria-expanded', 'false')
+  navbar.setAttribute('inert', '')
+});
 
 updateNavbar(media)
