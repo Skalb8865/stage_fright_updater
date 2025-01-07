124ff37fcb4ec4d1e930fd4267988f8385111c81
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-01-07 10:07:52 -0500

    .

--- src/cart.js
+++ src/cart.js
@@ -2,17 +2,13 @@ const cartIcon = document.querySelector("#cart-icon");
 const cart = document.querySelector(".cart");
 const cartClose = document.querySelector("#cart-close");
 
-cartIcon.addEventListener("click", () => cart.classList.add("active"),
-    cartIcon.setAttribute('aria-expanded', 'true'));
-cartClose.addEventListener("click", () => cart.classList.remove("active"),
-    cartIcon.setAttribute('aria-expanded', 'false'));
+cartIcon.addEventListener("click", () => cart.classList.add("active"));
+cartClose.addEventListener("click", () => cart.classList.remove("active"));
 
 const mobileNavIcon = document.querySelector("#mobile-nav--icon");
 const mobileNav = document.querySelector(".mobile-nav");
 const mobileNavClose = document.querySelector("#mobile-nav--close");
 
 
-mobileNavIcon.addEventListener("click", () => mobileNav.classList.add("active"),
-    mobileNavIcon.setAttribute('aria-expanded', 'true'));
-mobileNavClose.addEventListener("click", () => mobileNav.classList.remove("active"),
-    mobileNavIcon.setAttribute('aria-expanded', 'false'));
+mobileNavIcon.addEventListener("click", () => mobileNav.classList.add("active"));
+mobileNavClose.addEventListener("click", () => mobileNav.classList.remove("active"));
