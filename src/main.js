2cf273eef4e55c7ee54acfe36a2c1c1d8cd908ac
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-01-19 18:08:12 -0500

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/fonts.css'
+import '../styles/shopping.css'
+import '../styles/shopping.css'
+
 import './cart.js'
 import './swiper.js'
 import './tour.js'
