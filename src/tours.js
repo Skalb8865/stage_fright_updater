b8396cf0d6b5403b019df2230170b151072402da
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-04 16:52:44 -0500

    .

--- src/tours.js
+++ src/tours.js
@@ -1,5 +1,5 @@
 const tourCards = document.querySelector(".tours");
-const toursJson = "/data/tours.json";
+const toursJson = "/public/tours.json";
 
 // Modal elements
 const modal = document.getElementById("tourModal");
