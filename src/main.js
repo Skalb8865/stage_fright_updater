4bca56338cb875343f68a210ab892fea438d816a
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-10 21:41:02 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import '../styles/modern-normalize.css'
+// import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/fonts.css'
 import '../styles/shopping.css'
 
 import './cart.js'
-import './swiper.js'
+// import './swiper.js'
