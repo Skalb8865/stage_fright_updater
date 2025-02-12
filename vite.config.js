0266103cd995a04c94fb0b99ff03cf05f202647f
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-12 12:38:08 -0500

    .

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
         
         logo_long_sleeve: resolve(__dirname, 'html/Merch/Apparel/white_long_sleeve.html'),
         acantha_tshirt: resolve(__dirname, 'html/Merch/Apparel/acantha_T-shirt.html'),
-        acantha_hoodie: resolve(__dirname, 'html/Merch/Apparel/acantha_hoodie.html'),
+        acantha_hoodie: resolve(__dirname, 'html/Merch/Apparel/acantha_black_hoodie.html'),
         white_hat: resolve(__dirname, 'html/Merch/Apparel/white_logo_hat.html'),
         nyx_tshirt: resolve(__dirname, 'html/Merch/Apparel/nyx_T-shirt.html'),
         nyx_black_hoodie: resolve(__dirname, 'html/Merch/Apparel/nyx_black_hoodie.html'),
