92d723b8dbf410aad1bec7de03b1e71ac1aaa537
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-14 22:29:08 -0500

    .

--- src/merch.js
+++ src/merch.js
@@ -7,14 +7,14 @@ fetch(jsonFile)
     data.forEach((product) => {
       const { link, name, price, image } = product;
       merch.innerHTML += `
-        <div class="merch-box">
+      <div class="merch-box">
           <a href="${link}" aria-label="${name}"><img
           src="${image}" alt="${name}" class="product-img"></a>
           <div class="info-section">
             <h2 class="product-title">${name}</h2>
             <span class="product-price">${price}</span>
           </div>
-		</div>
+		  </div>
         `;
     });
   });
