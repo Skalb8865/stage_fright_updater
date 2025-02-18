93c54efbab014788986673095775cabac7bf23b9
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-18 05:26:54 -0500

    fixed logo centering and tested a new navbar

--- html/test/jsontest.js
+++ html/test/jsontest.js
@@ -1,23 +1,6 @@
-const mostPopPorducts = document.querySelector(".shop-content");
-
-const jsonFile = "/data/merch.json";
-
-fetch(jsonFile)
-    .then((respone) => {
-        return respone.json();
-    })
-    .then((data) => {
-        data.map((product) => {
-            const { id, link, name, price, images } = product;
-            mostPopPorducts.innerHTML += `
-        <div class="merch-box" data-product-id="${id}">
-          <a href="${link}" aria-label="${name}"><img
-          src="${images[0].url}" alt="${name}" class="product-img"></a>
-          <div class="info-section">
-            <h2 class="product-title">${name}</h2>
-            <span class="product-price">${price}</span>
-          </div>
-				</div>
-        `;
-        });
-    });
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  const hamburger = document.getElementById('hamburger');
+  menu.classList.toggle('active');
+  hamburger.classList.toggle('open');
+}
