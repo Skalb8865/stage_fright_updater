a16807aa899ae65cd38443885a041006c34167c1
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-18 22:28:18 -0500

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,28 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  function toggleMenu() {
-    const menu = document.getElementById('menu');
-    const hamburger = document.getElementById('hamburger');
-    menu.classList.toggle('active');
-    hamburger.classList.toggle('open');
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  const hamburger = document.getElementById('hamburger');
+  menu.classList.toggle('active');
+  hamburger.classList.toggle('open');
 
-    const noScroll = window.innerWidth > 1;
-    const isMenuOpen = menu.classList.contains("active");
+  const noScroll = window.innerWidth > 1;
+  const isMenuOpen = menu.classList.contains("active");
 
-    if (noScroll && (isMenuOpen)) {
-      document.body.style.overflow = "hidden";
-      document.getElementById("cart-icon").style.pointerEvents = "none";
-      hamburger.setAttribute("aria-expanded", "true");
-      menu.removeAttribute("inert");
-    } else {
-      document.body.style.overflow = "auto";
-      document.getElementById("cart-icon").style.pointerEvents = "auto";
-      hamburger.setAttribute("aria-expanded", "false");
-      menu.setAttribute("inert", "");
-    }
+  if (noScroll && (isMenuOpen)) {
+    document.body.style.overflow = "hidden";
+    document.getElementById("cart-icon").style.pointerEvents = "none";
+    hamburger.setAttribute("aria-expanded", "true");
+    menu.removeAttribute("inert");
+  } else {
+    document.body.style.overflow = "auto";
+    document.getElementById("cart-icon").style.pointerEvents = "auto";
+    hamburger.setAttribute("aria-expanded", "false");
+    menu.setAttribute("inert", "");
   }
-  document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    hamburger.addEventListener('click', toggleMenu);
-  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  hamburger.addEventListener('click', toggleMenu);
 });
 
