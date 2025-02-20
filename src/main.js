34c1c607b2dfdbbb1f2a1a1615ae567a4bcf19ba
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-20 13:10:18 -0500

    fixed a lot of things

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/fonts.css'
+import '../styles/modern-normalize.css'
 import '../styles/shopping.css'
 
 import './utils.js'
