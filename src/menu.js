5ab92cb13eef18ce8075a94cbedc1cd9fe54ee19
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-02-24 13:42:38 -0500

    changes
    fixed menu, fixed timeline, added menu_header to every page, added updated project plan

--- src/menu.js
+++ src/menu.js
@@ -1,51 +1,52 @@
 function toggleMenu() {
-  const menu = document.getElementById('menu');
-  const hamburger = document.getElementById('hamburger');
-  const navCenterLeft = document.querySelector('.center-left');
-  const navCenterRight = document.querySelector('.center-right');
-  const body = document.body;
+  const menu = document.getElementById("menu");
+  const hamburger = document.getElementById("hamburger");
+  const navCenterLeft = document.querySelector(".center-left");
+  const navCenterRight = document.querySelector(".center-right");
 
-  menu.classList.toggle('active');
-  hamburger.classList.toggle('open');
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("open");
 
   const noScroll = window.innerWidth > 1;
-  const isMenuOpen = menu.classList.contains('active');
-
-  // Add event listener for window resize
-  window.addEventListener('resize', () => {
-    if (isMenuOpen) {
-      body.style.overflow = 'hidden';
-    } else {
-      body.style.overflow = 'auto';
-    }
-  });
+  const isMenuOpen = menu.classList.contains("active");
 
   if (noScroll && isMenuOpen) {
-    body.style.overflow = 'hidden';
-    document.getElementById('cart-icon').style.pointerEvents = 'none';
-    hamburger.setAttribute('aria-expanded', 'true');
-    menu.removeAttribute('inert');
+    document.body.style.overflow = "hidden";
+    document.getElementById("cart-icon").style.pointerEvents = "none";
+    hamburger.setAttribute("aria-expanded", "true");
+    menu.removeAttribute("inert");
     setTimeout(() => {
-      navCenterLeft.style.display = 'none';
-      navCenterRight.style.display = 'none';
+      navCenterLeft.style.display = "none";
+      navCenterRight.style.display = "none";
     }, 250);
-
   } else {
-    body.style.overflow = 'auto';
-    document.getElementById('cart-icon').style.pointerEvents = 'auto';
-    hamburger.setAttribute('aria-expanded', 'false');
-    menu.setAttribute('inert', '');
+    document.body.style.overflow = "auto";
+    document.getElementById("cart-icon").style.pointerEvents = "auto";
+    hamburger.setAttribute("aria-expanded", "false");
+    menu.setAttribute("inert", "");
     setTimeout(() => {
-      navCenterLeft.style.display = 'flex';
-      navCenterRight.style.display = 'flex';
+      navCenterLeft.style.display = "flex";
+      navCenterRight.style.display = "flex";
     }, 250);
   }
-
-
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.getElementById('hamburger');
-  hamburger.addEventListener('click', toggleMenu);
-  menu.setAttribute('inert', '');
+function disableScroll() {
+  const noScroll = window.innerWidth > 1;
+  const isMenuOpen = menu.classList.contains("active");
+  // Add event listener for window resize
+  window.addEventListener("resize", () => {
+    if (noScroll && isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  hamburger.addEventListener("click", toggleMenu);
+  menu.setAttribute("inert", "");
+  window.addEventListener("resize", disableScroll);
 });
