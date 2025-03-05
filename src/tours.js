9a8ffcc72e5666e7f1d27f53d911000b76471802
unknown <PS25SKalb@efcts.us>
2025-03-05 10:01:12 -0500

    added an overlay to tour card

--- src/tours.js
+++ src/tours.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 const { date, location, place, buttonid } = tour;
                 tourCards.innerHTML += `
             <div class="tour-card">
+                <div class="tour_card-overlay"></div>
                 <p class="tour-dates">${date}</p>
                 <p class="tour-location">${location}</p>
                 <p class="tour-place">${place}</p>
