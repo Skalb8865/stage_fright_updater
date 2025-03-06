d51b628f962e23d3fc5b6b2c81c62e420aebf67c
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-03-06 02:04:59 -0500

    changes
    added merch sorter

--- src/merch.js
+++ src/merch.js
@@ -5,9 +5,9 @@ fetch(jsonFile)
   .then((response) => response.json())
   .then((data) => {
     data.forEach((product) => {
-      const { link, name, price, image } = product;
+      const { link, name, price, image, sort_attribute } = product;
       merch.innerHTML += `
-      <div class="merch-box">
+      <div class="merch-box merch_sort show ${sort_attribute}">
           <a href="${link}" aria-label="${name}"><img
           src="${image}" alt="${name}" class="product-img"></a>
           <div class="info-section">
